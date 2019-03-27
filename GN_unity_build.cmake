22fe9728e7aa3d2a96fb15feb109278e6eef7d12
bomkvilt <kvilt-d.max@yandex.ru>
2019-03-27 18:31:28 +0300

    bugfix

--- GN_unity_build.cmake
+++ GN_unity_build.cmake
@@ -2,15 +2,14 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~| unity building
 
 macro(GN_init_unity)
-    # skip unity build
     if (NOT GN_bUnity)
+        # skip unity build
         GN_debug("unity build skipping...")
-        return()
+    else()
+        # download cotire
+        GN_Download_project(root "cotire" "${GN_dir_gremlin}/GN_loader_cotire.cmake")
+        include("${root}/src/CMake/cotire.cmake")
         endif()
-    
-    # download cotire
-    GN_Download_project(root "cotire" "${GN_dir_gremlin}/GN_loader_cotire.cmake")
-    include("${root}/src/CMake/cotire.cmake")
     endmacro()
 
 function(GN_unity_build this)
